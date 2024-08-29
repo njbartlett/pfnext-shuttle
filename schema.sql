997b17911679b61973de84cb7204e3c99847ce4a
Neil Bartlett <njbartlett@gmail.com>
2024-08-29 23:18:48 +0100

    Add tests for creating bookings

--- schema.sql
+++ schema.sql
@@ -30,23 +30,39 @@ VALUES
 ON CONFLICT DO NOTHING;
 
 -- session tables
-CREATE TABLE IF NOT EXISTS session_type (
-    id serial PRIMARY KEY,
-    name varchar(255) UNIQUE NOT NULL
+CREATE TABLE public.session_type (
+	id serial4 NOT NULL,
+	name varchar(255) NOT NULL,
+	requires_trainer bool DEFAULT true NULL,
+	cost int2 DEFAULT 0 NULL,
+	CONSTRAINT session_type_cost_check CHECK ((cost >= 0)),
+	CONSTRAINT session_type_name_key UNIQUE (name),
+	CONSTRAINT session_type_pkey PRIMARY KEY (id)
 );
+INSERT INTO session_type
+    (name, cost)
+VALUES
+    ('HIIT', 1),
+    ('Strong', 1),
+    ('On The Move', 1)
+ON CONFLICT DO NOTHING;
 
-CREATE TABLE IF NOT EXISTS session (
-    id bigserial PRIMARY KEY,
-    datetime timestamp with time zone NOT NULL,
-    duration_mins integer NOT NULL,
-    session_type integer NOT NULL REFERENCES session_type,
-    location integer NOT NULL REFERENCES location,
-    trainer bigint NOT NULL REFERENCES person,
-    max_booking_count BIGINT
+CREATE TABLE IF NOT EXISTS session(
+	id bigserial PRIMARY KEY,
+	datetime timestamptz NOT NULL,
+	duration_mins int4 NOT NULL,
+	session_type int4 NOT NULL REFERENCES session_type,
+	location int4 NULL REFERENCES location,
+	trainer int8 NULL REFERENCES person,
+	max_booking_count int8 NULL,
+	notes text NULL,
+	cost int2 DEFAULT 0 NOT NULL CHECK ((cost >= 0))
 );
 
 CREATE TABLE IF NOT EXISTS booking (
     person_id bigint NOT NULL REFERENCES person ON DELETE CASCADE,
     session_id bigint NOT NULL REFERENCES session ON DELETE CASCADE,
+    attended bool DEFAULT false NOT NULL,
+	credits_used int2 DEFAULT 0 NULL CHECK ((credits_used >= 0)),
     PRIMARY KEY (person_id, session_id)
 );
--- src/bookings.rs
+++ src/bookings.rs
@@ -1,4 +1,5 @@
 use chrono::{Datelike, DateTime, Days, NaiveTime, TimeZone, Utc};
+use chrono_tz::Tz;
 use rocket::futures::StreamExt;
 use rocket::futures::stream::BoxStream;
 use rocket::http::Status;
@@ -7,13 +8,13 @@ use rocket::serde::json::Json;
 use rocket::serde::Serialize;
 use rocket::State;
 use serde::Deserialize;
-use sqlx::{Error, FromRow, PgPool, query_as, QueryBuilder, raw_sql, Row};
+use sqlx::{Error, Executor, FromRow, PgPool, query_as, QueryBuilder, raw_sql, Row};
 use sqlx::postgres::{PgQueryResult, PgRow};
 
 use crate::{AppState, parse_opt_date, SessionLocation, SessionType};
 use crate::claims::Claims;
 
-#[derive(Serialize, Deserialize, FromRow, Debug)]
+#[derive(Serialize, Deserialize, FromRow, Debug, Clone)]
 pub struct SessionBooking {
     person_id: i64,
     session_id: i64
@@ -72,6 +73,17 @@ pub async fn list_bookings(
     from: Option<String>,
     to: Option<String>
 ) -> Result<Json<Vec<SessionBookingFull>>, Custom<String>> {
+    _list_bookings(&state.pool, &claim, session_id, person_id, from, to).await
+}
+
+async fn _list_bookings(
+    pool: &PgPool,
+    claim: &Claims,
+    session_id: Option<i64>,
+    person_id: Option<i64>,
+    from: Option<String>,
+    to: Option<String>
+) -> Result<Json<Vec<SessionBookingFull>>, Custom<String>> {
     let mut qb = QueryBuilder::new("SELECT b.person_id, p.name AS person_name, p.email AS person_email, b.session_id, \
                 s.datetime AS session_datetime, s.duration_mins AS session_duration_mins, s.location AS session_location_id, l.name AS session_location_name, l.address AS session_location_address, \
                 s.session_type AS session_type_id, t.name AS session_type_name, t.requires_trainer AS session_type_requires_trainer, t.cost AS session_type_cost, b.attended \
@@ -112,7 +124,7 @@ pub async fn list_bookings(
     qb.push(" ORDER BY session_datetime, person_name");
     info!("list_bookings compiled SQL: {}", qb.sql());
     let bookings = qb.build_query_as()
-        .fetch_all(&state.pool)
+        .fetch_all(pool)
         .await
         .map_err(|e| Custom(Status::InternalServerError, e.to_string()))?;
     Ok(Json(bookings))
@@ -127,6 +139,9 @@ async fn take_result_from_stream<'a>(stream: &mut BoxStream<'a, Result<PgQueryRe
 
 #[post("/bookings", data="<booking>")]
 pub async fn create_booking(state: &State<AppState>, claim: Claims, booking: Json<SessionBooking>) -> Result<Created<Json<SessionBooking>>, Custom<String>> {
+    _create_booking(&state.pool, &state.timezone, &claim, booking).await
+}
+async fn _create_booking(pool: &PgPool, timezone: &Tz, claim: &Claims, booking: Json<SessionBooking>) -> Result<Created<Json<SessionBooking>>, Custom<String>> {
     // Admins can always make a booking for any user
     if !claim.has_role("admin") {
         // Members can only book on their own behalf
@@ -142,7 +157,7 @@ pub async fn create_booking(state: &State<AppState>, claim: Claims, booking: Jso
         }
 
         // Non-admins can only book future sessions
-        let session_date_and_cost = get_session_date_and_cost(&state.pool, &booking.session_id).await?;
+        let session_date_and_cost = get_session_date_and_cost(pool, &booking.session_id).await?;
         if session_date_and_cost.datetime.lt(&Utc::now()) {
             info!("person id {} attempted to book session in past (session id {}, date {}); denied: missing admin role", claim.uid, session_date_and_cost.id, session_date_and_cost.datetime);
             return Err(Custom(Status::Forbidden, "cannot create booking in the past".to_string()));
@@ -150,22 +165,22 @@ pub async fn create_booking(state: &State<AppState>, claim: Claims, booking: Jso
 
         // Limited members can only book if they have no other payable sessions in the same week
         if claim.has_role("limited-member") {
-            check_limited_member_has_no_bookings_in_same_week(&state, claim.uid, &session_date_and_cost).await?;
+            check_limited_member_has_no_bookings_in_same_week(pool, timezone, claim.uid, &session_date_and_cost).await?;
         }
     }
 
     // Read the max_booking_count for the session if present
     let session_with_max_booking_count: SessionWithMaxBookingCount = query_as("SELECT id, max_booking_count FROM session WHERE id = $1")
         .bind(&booking.session_id)
-        .fetch_optional(&state.pool)
+        .fetch_optional(pool)
         .await
         .map_err(|e| Custom(Status::InternalServerError, e.to_string()))?
         .ok_or(Custom(Status::NotFound, format!("no session with id {}", &booking.session_id)))?;
 
     // Make the booking
     match session_with_max_booking_count.max_booking_count {
-        Some(max_booking_count) => book_session_with_max_bookings(state, booking.person_id, booking.session_id, max_booking_count).await,
-        None => book_session_no_max_bookings(state, booking.person_id, booking.session_id).await
+        Some(max_booking_count) => book_session_with_max_bookings(pool, booking.person_id, booking.session_id, max_booking_count).await,
+        None => book_session_no_max_bookings(pool, booking.person_id, booking.session_id).await
     }?;
 
     info!("Created booking: {:?}", &booking);
@@ -186,14 +201,14 @@ struct MemberExistingBooking {
     datetime: DateTime<Utc>
 }
 
-async fn check_limited_member_has_no_bookings_in_same_week(state: &State<AppState>, uid: i64, session_date_and_cost: &SessionDateAndCost) -> Result<(), Custom<String>> {
+async fn check_limited_member_has_no_bookings_in_same_week(pool: &PgPool, timezone: &Tz, uid: i64, session_date_and_cost: &SessionDateAndCost) -> Result<(), Custom<String>> {
     // Can always book a zero-cost session even if you already have other bookings.
     if session_date_and_cost.cost == 0 {
         return Ok(());
     }
 
     // Get the date/time of the session and work out the start and end of the week that the session occurs in
-    let datetime_in_local = &state.timezone.from_utc_datetime(&session_date_and_cost.datetime.naive_utc());
+    let datetime_in_local = timezone.from_utc_datetime(&session_date_and_cost.datetime.naive_utc());
     let start_of_week_local = datetime_in_local
         .checked_sub_days(Days::new(datetime_in_local.weekday().num_days_from_monday() as u64)).unwrap()
         .with_time(NaiveTime::from_hms_opt(0, 0, 0).unwrap())
@@ -212,7 +227,7 @@ async fn check_limited_member_has_no_bookings_in_same_week(state: &State<AppStat
         .bind(uid)
         .bind(start_of_week_local)
         .bind(end_of_week_local)
-        .fetch_all(&state.pool)
+        .fetch_all(pool)
         .await
         .map_err(|e| Custom(Status::InternalServerError, e.to_string()))?;
 
@@ -224,11 +239,11 @@ async fn check_limited_member_has_no_bookings_in_same_week(state: &State<AppStat
     Ok(())
 }
 
-async fn book_session_no_max_bookings(state: &State<AppState>, person_id: i64, session_id: i64) -> Result<(), Custom<String>> {
+async fn book_session_no_max_bookings(pool: &PgPool, person_id: i64, session_id: i64) -> Result<(), Custom<String>> {
     query_as("INSERT INTO booking (person_id, session_id) VALUES ($1, $2) RETURNING person_id, session_id")
         .bind(person_id)
         .bind(session_id)
-        .fetch_one(&state.pool)
+        .fetch_one(pool)
         .await
         .map_err(|e| Custom(Status::InternalServerError, e.to_string()))
 }
@@ -240,7 +255,7 @@ struct SessionWithMaxBookingCount {
 }
 
 
-async fn book_session_with_max_bookings(state: &State<AppState>, person_id: i64, session_id: i64, max_bookings: i64) -> Result<(), Custom<String>> {
+async fn book_session_with_max_bookings(pool: &PgPool, person_id: i64, session_id: i64, max_bookings: i64) -> Result<(), Custom<String>> {
     // Atomically update the booking table to insert a new booking if and only if the count of
     // bookings for the referenced session is less than the maximum. Adapted from this StackOverflow
     // answer: https://dba.stackexchange.com/a/167283
@@ -256,7 +271,7 @@ async fn book_session_with_max_bookings(state: &State<AppState>, person_id: i64,
         RETURNING person_id, session_id; \
         END;", session_id, person_id, session_id, session_id, max_bookings);
     info!("Executing raw SQL: {}", &sql);
-    let mut result_stream = raw_sql(sql.as_str()).execute_many(&state.pool);
+    let mut result_stream = raw_sql(sql.as_str()).execute_many(pool);
 
     let _ = take_result_from_stream(&mut result_stream).await?; // result from BEGIN;
     let _ = take_result_from_stream(&mut result_stream).await?; // result from SELECT..FOR UPDATE;
@@ -281,19 +296,23 @@ async fn get_session_date_and_cost(pool: &PgPool, session_id: &i64) -> Result<Se
 
 #[delete("/bookings?<session_id>&<person_id>")]
 pub async fn delete_booking(state: &State<AppState>, claim: Claims, person_id: i64, session_id: i64) -> Result<Json<SessionBooking>, Custom<String>> {
+    _delete_booking(&state.pool, &claim, person_id, session_id).await
+}
+
+async fn _delete_booking(pool: &PgPool, claim: &Claims, person_id: i64, session_id: i64) -> Result<Json<SessionBooking>, Custom<String>> {
     if !claim.has_role("admin") {
         if person_id != claim.uid {
             return Err(Custom(Status::Forbidden, "not allowed to cancel bookings for other users".to_string()));
         }
         // Error if session is in the past
-        if get_session_date_and_cost(&state.pool, &session_id).await?.datetime.lt(&Utc::now()) {
+        if get_session_date_and_cost(pool, &session_id).await?.datetime.lt(&Utc::now()) {
             return Err(Custom(Status::Forbidden, "cannot cancel past booking".to_string()));
         }
     }
     let booking_deleted = query_as("DELETE FROM booking WHERE person_id = $1 AND session_id = $2 RETURNING person_id, session_id")
         .bind(person_id)
         .bind(session_id)
-        .fetch_optional(&state.pool)
+        .fetch_optional(pool)
         .await
         .map_err(|e| Custom(Status::InternalServerError, e.to_string()))?
         .ok_or(Custom(Status::NotFound, format!("no booking found with person_id={} and session_id={}", person_id, session_id)))?;
@@ -374,3 +393,169 @@ pub async fn get_attendance_stats(state: &State<AppState>, claim: Claims, from: 
 
     Ok(Json(stats))
 }
+
+#[cfg(test)]
+mod tests {
+    use std::ops::Add;
+    use chrono::{DateTime, Duration, TimeDelta, Utc};
+    use chrono_tz::Tz;
+    use rocket::http::Status;
+    use rocket::serde::json::Json;
+    use rocket::response::status::Custom;
+    use sqlx::{Executor, FromRow, PgPool, query_as};
+    use crate::bookings::{_delete_booking, SessionBooking};
+    use crate::claims::Claims;
+
+    #[derive(FromRow)]
+    struct IntRecord {
+        id: i32
+    }
+
+    #[derive(FromRow)]
+    struct BigintRecord {
+        id: i64
+    }
+
+    async fn create_person(pool: &PgPool, email: &str, roles: &str) -> i64 {
+        let member_id: BigintRecord = query_as("insert into person (name, email, roles) values ('Test User', $1, $2) returning id")
+            .bind(email)
+            .bind(roles)
+            .fetch_one(pool)
+            .await.unwrap();
+        member_id.id
+    }
+
+    async fn create_session(pool: &PgPool, datetime: &DateTime<Utc>, trainer_id: i64, session_type_name: &str, location_name: &str) -> i64 {
+        let session_type_id: IntRecord = query_as("select id from session_type where name = $1")
+            .bind(session_type_name)
+            .fetch_one(pool).await.unwrap();
+
+        let location_id: IntRecord = query_as("select id from location where name = $1")
+            .bind(location_name)
+            .fetch_one(pool).await.unwrap();
+
+        let session_id_record: BigintRecord = query_as("insert into session (datetime, duration_mins, session_type, location, trainer, cost) \
+            VALUES ($1, 60, $2, $3, $4, 1) \
+            RETURNING id
+        ")
+            .bind(datetime)
+            .bind(session_type_id.id)
+            .bind(location_id.id)
+            .bind(trainer_id)
+            .fetch_one(pool).await.unwrap();
+
+        session_id_record.id
+    }
+
+    #[sqlx::test]
+    async fn book_session_full_member(pool: PgPool) {
+        pool.execute(include_str!("../schema.sql")).await.unwrap();
+
+        let trainer_id = create_person(&pool, "trainer@example.org", "member,trainer").await;
+        let member_id = create_person(&pool, "member@example.org", "member").await;
+        let session_id = create_session(&pool, &Utc::now().add(TimeDelta::days(1)), trainer_id, "HIIT", "Oak Hill Park").await;
+        let booking = SessionBooking {
+            person_id: member_id,
+            session_id
+        };
+
+        // Precondition: zero bookings
+        assert_eq!(0, count_bookings(&pool).await);
+
+        // Create booking
+        let timezone: Tz = "Europe/London".parse().unwrap();
+        let claim = Claims::create(member_id, "joe@example.com", &Some("011111".to_string()), &vec!["member".to_string()], Duration::minutes(1));
+        crate::bookings::_create_booking(&pool, &timezone, &claim, Json(booking)).await.unwrap();
+
+        // Postcondition: 1 booking
+        assert_eq!(1, count_bookings(&pool).await);
+    }
+
+    #[sqlx::test]
+    async fn book_session_non_member(pool: PgPool) {
+        pool.execute(include_str!("../schema.sql")).await.unwrap();
+
+        let trainer_id = create_person(&pool, "trainer@example.org", "member,trainer").await;
+        let member_id = create_person(&pool, "member@example.org", "member").await;
+        let session_id = create_session(&pool, &Utc::now().add(TimeDelta::days(1)), trainer_id, "HIIT", "Oak Hill Park").await;
+        let booking = SessionBooking {
+            person_id: member_id,
+            session_id
+        };
+
+        // Precondition: zero bookings
+        assert_eq!(0, count_bookings(&pool).await);
+
+        // Create booking
+        let timezone: Tz = "Europe/London".parse().unwrap();
+        let claim = Claims::create(member_id, "joe@example.com", &Some("011111".to_string()), &vec![], Duration::minutes(1));
+        let result = crate::bookings::_create_booking(&pool, &timezone, &claim, Json(booking)).await;
+        assert!(result.is_err());
+        assert_eq!(Custom(Status::Forbidden, "missing or expired membership".to_string()), result.err().unwrap());
+
+        // Postcondition: still zero bookings
+        assert_eq!(0, count_bookings(&pool).await);
+    }
+
+    #[sqlx::test]
+    async fn book_session_limited_member(pool: PgPool) {
+        pool.execute(include_str!("../schema.sql")).await.unwrap();
+
+        let trainer_id = create_person(&pool, "trainer@example.org", "member,trainer").await;
+        let member_id = create_person(&pool, "member@example.org", "limited-member").await;
+        let datetime = Utc::now().add(TimeDelta::days(1));
+        let session_id_1 = create_session(&pool, &datetime, trainer_id, "HIIT", "Oak Hill Park").await;
+        let booking_1 = SessionBooking {
+            person_id: member_id,
+            session_id: session_id_1
+        };
+        let session_id_2 = create_session(&pool, &datetime, trainer_id, "On The Move", "Oak Hill Park").await;
+        let booking_2 = SessionBooking {
+            person_id: member_id,
+            session_id: session_id_2
+        };
+        let timezone: Tz = "Europe/London".parse().unwrap();
+
+        // Precondition: zero bookings
+        assert_eq!(0, count_bookings(&pool).await);
+
+        // Create booking 1
+        let claim = Claims::create(member_id, "member@example.com", &Some("011111".to_string()), &vec!["limited-member".to_string()], Duration::minutes(1));
+        crate::bookings::_create_booking(&pool, &timezone, &claim, Json(booking_1)).await.unwrap();
+
+        // Postcondition 1: one booking
+        assert_eq!(1, count_bookings(&pool).await);
+
+        // Create booking 2: fails
+        let claim = Claims::create(member_id, "member@example.com", &Some("011111".to_string()), &vec!["limited-member".to_string()], Duration::minutes(1));
+        let result = crate::bookings::_create_booking(&pool, &timezone, &claim, Json(booking_2.clone())).await;
+        assert!(result.is_err());
+        assert_eq!(Custom(Status::Forbidden, "cannot book session: member already has 1 booking(s) in this week".to_string()), result.err().unwrap());
+
+        // Postcondition 2: one booking
+        assert_eq!(1, count_bookings(&pool).await);
+
+        // Cancel booking 1
+        _delete_booking(&pool, &claim, member_id, session_id_1).await.unwrap();
+
+        // Postcondition 3: zero bookings
+        assert_eq!(0, count_bookings(&pool).await);
+
+        // Create booking 2: succeeds now
+        let claim = Claims::create(member_id, "member@example.com", &Some("011111".to_string()), &vec!["limited-member".to_string()], Duration::minutes(1));
+        crate::bookings::_create_booking(&pool, &timezone, &claim, Json(booking_2)).await.unwrap();
+
+        // Postcondition 4: one booking
+        assert_eq!(1, count_bookings(&pool).await);
+    }
+
+
+    async fn count_bookings(pool: &PgPool) -> i64 {
+        let record: BigintRecord = query_as("select count(*) as id from booking")
+            .fetch_one(pool)
+            .await
+            .unwrap();
+        record.id
+    }
+}
+
