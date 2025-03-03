cdf4e08e993ca9d2f7a28516716aed43b417b1a4
Neil Bartlett <njbartlett@gmail.com>
2025-03-03 14:39:44 +0000

    Remove top-10 limit on attendance stats

--- src/bookings.rs
+++ src/bookings.rs
@@ -511,8 +511,7 @@ pub async fn get_attendance_stats(state: &State<AppState>, claim: Claims, from: 
 
     qb.push(") AS attended_count \
         FROM person AS p \
-        ORDER BY attended_count DESC, name \
-        LIMIT 10");
+        ORDER BY attended_count DESC, name");
     info!("fetching: {}", qb.sql());
 
     let stats = qb.build_query_as()
